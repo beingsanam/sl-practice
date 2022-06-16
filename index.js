3f41931a3fbde5d6e9d55331768a75c4143fa97e
Suman <sumanbhandari0320@gmail.com>
2022-06-16 11:30:29 +0545

    concept

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 3;
+console.log(a < b); //false
+console.log(a > b); //true
+console.log(a <= b); //false
+console.log(a >= b); //true
+console.log(a == b); //false
+console.log(5 == 5); //true
+console.log(5 == "5"); //true
+console.log(parseInt("2") + parseInt("3"));
+console.log(5 === 5); //true
+console.log(5 === parseInt("5"));
+console.log(5 != 5); //false
+console.log(5 !== "5");
