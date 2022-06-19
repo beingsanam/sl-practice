6be95a9c07f7a10d6fa62efe8201d19f72e17739
Suman <sumanbhandari0320@gmail.com>
2022-06-19 21:29:37 +0545

    day-4

---
+++ index.js
@@ -0,0 +1,100 @@
+//Control Structures- Control the flow of program
+//1. Conditional - used to make decison on which section of code/program to run or execute
+//if
+//if - syntax
+//if(condition){
+//statements;
+//}
+//if-else if - else
+//syntax
+// if (condition) {
+//   statements;
+// } else if (condition) {
+//   statements;
+// } else {
+//   statements;
+// }
+//switch
+// switch (expression) {
+//   case value:
+//     statements;
+//     break;
+
+//   default:
+//     statemnets;
+// }
+//2. Loops
+//do while
+//while
+//for
+
+// let age = 10;
+// if (age > 18) {
+//   console.log("You can vote");
+// } else {
+//   console.log("you cannot vote");
+// }
+
+// let marks = 75;
+// if (marks > 80) {
+//   console.log("A+");
+// } else if (marks > 70) {
+//   console.log("B+");
+// } else {
+//   console.log("Average");
+// }
+
+// let num = prompt("Enter name");
+// console.log(num);
+
+// let num = parseInt(prompt("Enter num"));
+
+// switch (num) {
+//   case 1:
+//     console.log("1 aayo");
+//     break;
+//   case 2:
+//     console.log("2 aayo");
+//     break;
+//   case 3:
+//     console.log("3 aayo");
+//     break;
+//   default:
+//     console.log("3 bhnada mathi");
+// }
+
+//use of return
+
+// function suman() {
+//   console.log("1");
+//   return;
+//   console.log("2");
+// }
+// suman();
+
+//for loop
+//for(initialization;condition;increment/decrement){
+//   statements
+// }
+// let i;
+// for (i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+//do while
+// do {
+//   statements;
+// } while (condition);
+
+// let i;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+//do while runs at least once even if the condition doesn't match
+// let i;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+//while loop runs only if the condition matches
